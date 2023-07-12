3c0c8b35d5286af7e229b3f32d656348dd3b1d26
jenea130 <jenea130@mail.ru>
2023-07-12 21:22:34 +0300

    trainers: start-trainers-slider

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const slider = $(".trainers__wrap")
+
+    if (slider) {
+        trainersSlider()
+    }
+
+    function trainersSlider() {
+        slider.slick({
+            slidesToShow:3,
+            slidesToScroll:3,
+            arrows:false
+        })
+    }
+});
+

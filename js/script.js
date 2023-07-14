c4ccd90307f380a90b33dcad2055d4c0c5d0fbc3
jenea130 <jenea130@mail.ru>
2023-07-14 20:59:03 +0300

    trainers: stiles

--- js/script.js
+++ js/script.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
         slider.slick({
             slidesToShow:3,
             slidesToScroll:3,
-            arrows:false
+            arrows:false,
+            dots: true
         })
     }
 });

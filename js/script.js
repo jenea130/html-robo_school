b229b2b77a2dd67264146f26ad8bad8546e3c187
jenea130 <jenea130@mail.ru>
2023-07-17 19:55:33 +0300

    trainers: finished

--- js/script.js
+++ js/script.js
@@ -8,9 +8,18 @@ $(document).ready(function() {
     function trainersSlider() {
         slider.slick({
             slidesToShow:3,
-            slidesToScroll:3,
+            slidesToScroll:1,
+            speed: 1000,
             arrows:false,
             dots: true
+        })
+        const trainers__arrow_prev = $('.trainers__arrow--prev')
+        const trainers__arrow_next = $('.trainers__arrow--next')
+        trainers__arrow_prev.on('click', function() {
+            slider.slick('slickPrev')
+        })
+        trainers__arrow_next.on('click', function() {
+            slider.slick('slickNext')
         })
     }
 });
